e40d6a7ab333084d4fda75d7232709aecc3f1d52
pescador1 <32376793+pescador1@users.noreply.github.com>
2026-08-03 19:55:37 +0100

    Create constants.dart

---
+++ lib/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  // TODO: Update this to match the actual folder path of your PHP backend
+  static const String apiUrl = 'https://app.hr-tech.site/apk iptv/api_mobile.php';
+  
+  // App Colors
+  static const Color primaryColor = Color(0xFF3B82F6);
+  static const Color accentColor = Color(0xFF8B5CF6);
+  static const Color bgColor = Color(0xFF0F172A);
+  static const Color cardColor = Color(0xFF1E293B);
+  static const Color textMain = Color(0xFFF8FAFC);
+  static const Color textMuted = Color(0xFF94A3B8);
+}
